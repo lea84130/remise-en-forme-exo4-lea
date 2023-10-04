71be57bece09c5e4af4ef53504a9aaa775747153
Lea Bruneau <blea84130@gmail.com>
2023-10-04 18:11:21 +0200

    j'ai envoyé le js

---
+++ fichier4.js
@@ -0,0 +1,36 @@
+let displayValue = '';
+
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = displayValue;
+}
+
+function calculateResult() {
+    try {
+        const result = eval(displayValue);
+        document.getElementById('display').value = result;
+        displayValue = result.toString();
+    } catch (error) {
+        document.getElementById('display').value = 'Erreur';
+    }
+}
+
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = displayValue;
+}
+
+function calculateResult() {
+    try {
+        const result = eval(displayValue);
+        document.getElementById('display').value = result;
+        displayValue = result.toString();
+    } catch (error) {
+        document.getElementById('display').value = 'Erreur';
+    }
+}
